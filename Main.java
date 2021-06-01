ae7ade16231afd14d8734b51eb42a019bb30f2fb
venkateshnaik064 <85158891+venkateshnaik064@users.noreply.github.com>
2021-06-01 15:00:54 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+//This is a single line comment
+
+/*
+This is
+multi line
+comment
+ */
+
+/**
+ * This
+ * is documentation comment
+ */
+
+package com.company;
+
+public class Main {
+// programs that prints my name
+    public static void main(String[] args) {
+        // prints  my name
+        System.out.println("Mudavath Venkatesh");
+
+        //prints data types
+        boolean b = true;
+        if (b == true)
+            System.out.println("Hi jala technologies");
+        char a = 'G';
+        int i = 89;
+        float f = (float) 5.656;
+        double d = 4.684;
+        System.out.println("char: " + a);
+        System.out.println("integer: " + i);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+
+    }
+}
+
+
