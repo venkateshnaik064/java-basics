3186a2bcc18245363a3c167274aba11813326393
venkateshnaik064 <85158891+venkateshnaik064@users.noreply.github.com>
2021-06-01 15:16:56 +0530

    Add files via upload

---
+++ Java2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Java2 {
+    static void myMethod() {
+        System.out.println("Mudavath Venkatesh");
+        }
+        public static void main(String[] args) {
+            myMethod();
+        }
+    }
+
